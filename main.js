0b85a36564b0a7b625983280e8159c228b669800
Franklin <franklinokolie86@gmail.com>
2020-09-20 15:33:13 +0100

    Changed the ID to index.html

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var menu= document.getElementById('menu-icon');
     var menuList= document.getElementById('menu-list')
     var cancel= document.getElementById('cancel')
+    var listNav=document.getElementById("list-nav")
 
     menu.addEventListener("click" , function(){
       menuList.classList.add('showlist')
