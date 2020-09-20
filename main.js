5c5c684186a80d55a5c20527f4e81eb30c707ae3
Franklin <franklinokolie86@gmail.com>
2020-09-20 15:56:57 +0100

    Added responsive Navigation

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var menu= document.getElementById('menu-icon');
     var menuList= document.getElementById('menu-list')
     var cancel= document.getElementById('cancel')
-    var listNav=document.getElementById("list-nav")
+    
+    var contactNav=document.getElementById('contact-nav')
+    var serviceNav=document.getElementById('service-nav')
+    var aboutNav=document.getElementById('about-nav')
 
     menu.addEventListener("click" , function(){
       menuList.classList.add('showlist')
@@ -10,7 +13,20 @@ var menu= document.getElementById('menu-icon');
 
     cancel.addEventListener('click', function(){
       menuList.classList.remove('showlist')
-    })
+    });
+
+    contactNav.addEventListener('click', function(){
+      menuList.classList.remove('showlist')
+    });
+
+    serviceNav.addEventListener('click', function(){
+      menuList.classList.remove('showlist')
+    });
+
+    aboutNav.addEventListener('click', function(){
+      menuList.classList.remove('showlist')
+    });
+
 
 
 
